887907fe8810ec7454f2e4be5d2509ff502828aa
KrisMatrix <69052620+KrisMatrix@users.noreply.github.com>
2022-11-06 12:40:01 -0500

    Update p43.

--- p43_multiply_strings.py
+++ p43_multiply_strings.py
@@ -2,7 +2,9 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         return str(self.convert_string_to_int(num1) * self.convert_string_to_int(num2))
         
-    def convert_string_to_int(self, num):
+    def convert_string_to_int2(self, num):
+        # O(n) time complexity where n is length of string
+        # O(1) space complexity for 10 records
         hash = {
             "1": 1,
             "2": 2,
@@ -18,4 +20,12 @@ class Solution:
         retval = 0
         for i in range(len(num)):
             retval = 10 * retval + hash[num[i]]
+        return retval
+    
+    def convert_string_to_int(self,num):
+        # O(n) time complexity where n is length of string
+        # Constant complexity
+        retval = 0
+        for i in range(len(num)):
+            retval = 10 * retval + ord(num[i]) - 48
         return retval
