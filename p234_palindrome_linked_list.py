13a519ccb32dd9c0b80d191d76a943068a9ad785
KrisMatrix <69052620+KrisMatrix@users.noreply.github.com>
2022-11-11 20:33:53 -0500

    Update

--- p234_palindrome_linked_list.py
+++ p234_palindrome_linked_list.py
@@ -16,3 +16,39 @@ class Solution:
             if array[i] != array[len(array) - 1 - i]:
                 return False
         return True
+
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        #1. Get the mid point of linked list
+        #2. Reverse the second half of ll
+        #3. Step through the reverse of second half and check first half.
+        #4. If all match, then true, other wise false.
+        mid = self.get_mid_of_ll(head)
+        head2 = self.reverse_ll(mid)
+        while head2:
+            if head2.val != head.val:
+                return False
+            head2 = head2.next
+            head = head.next
+        return True
+
+    def get_mid_of_ll(self,head):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def reverse_ll(self,head):
+        reverse = ListNode()
+        while head:
+            reverse.val = head.val
+            reverse = ListNode(None, reverse)
+            head = head.next
+        return reverse.next
